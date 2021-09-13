5775c29fb0b00773706c3c5f97d521178c766f58
tuantu78 <89456437+tuantu78@users.noreply.github.com>
2021-09-13 21:52:29 +0700

    Add files via upload

---
+++ baocaoCNPM/data/products.js
@@ -0,0 +1,485 @@
+var list_products = [{
+    "name": "Samsung Galaxy S21+ 5G 128GB",
+    "company": "Samsung",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/226385/samsung-galaxy-s21-plus-tim-1-org.jpg",
+    "price": "16.990.000",
+    "star": 3,
+    "rateCount": 84,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Dynamic AMOLED 2X, 6.7', Full HD+",
+        "os": "Android 11",
+        "camara": "Chính 12 MP & Phụ 64 MP, 12 MP",
+        "camaraFront": "10 MP",
+        "chip": "Exynos 2100",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "sim": "2 Nano SIM hoặc 1 Nano SIM + 1 eSIM, Hỗ trợ 5G",
+        "battery": "4800 mAh, 25W"
+    },
+    "masp": "Sam0"
+}, {
+    "name": "Xiaomi Mi 8 Lite",
+    "company": "Xiaomi",
+    "img": "img/products/xiaomi-mi-8-lite-black-1-600x600.jpg",
+    "price": "6.690.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "chip": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "sim": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Xia0"
+}, {
+    "name": "OPPO RENO6 Z 5G",
+    "company": "Oppo",
+    "img": "https://cdn.tgdd.vn/2021/07/campaign/SP-390x456-1.png",
+    "price": "9.490.000",
+    "star": 4,
+    "rateCount": 188,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "AMOLED, 6.43', Full HD+",
+        "os": "	Android 11",
+        "camara": "	Chính 64 MP & Phụ 8 MP, 2 MP",
+        "camaraFront": "32 MP",
+        "chip": "MediaTek Dimensity 800U 5G",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "sim": "2 Nano SIM, Hỗ trợ 5G",
+        "battery": "4310 mAh, 30W"
+    },
+    "masp": "Opp0"
+}, {
+    "name": "Xiaomi Mi 11 Lite ",
+    "company": "Xiaomi",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/233241/xiaomi-mi-11-lite-4g-hong-1-org.jpg",
+    "price": "7.090.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "900.000"
+    },
+    "detail": {
+        "screen": "AMOLED, 6.55', Full HD+",
+        "os": "Android 11",
+        "camara": "Chính 64 MP & Phụ 8 MP, 5 MP",
+        "camaraFront": "16 MP",
+        "chip": "Snapdragon 732G",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "sim": "2 Nano SIM (SIM 2 chung khe thẻ nhớ), Hỗ trợ 4G",
+        "battery": "4250 mAh, 33W"
+    },
+    "masp": "Xia1"
+}, {
+    "name": "iPhone 12 Pro Max 128GB",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/213033/iphone-12-pro-max-xanh-duong-1-org.jpg",
+    "price": "32.990.000",
+    "star": 4,
+    "rateCount": 422,
+    "promo": {
+        "name": "giareonline",
+        "value": "30.990.000₫"
+    },
+    "detail": {
+        "screen": "OLED, 6.7', Super Retina XDR",
+        "os": "iOS 14",
+        "camara": "3 camera 12 MP",
+        "camaraFront": "12 MP",
+        "chip": "Apple A14 Bionic",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "sim": "1 Nano SIM & 1 eSIM, Hỗ trợ 5G",
+        "battery": "3687 mAh, 20W"
+    },
+    "masp": "App0"
+}, {
+    "name": "OPPO Find X3 Pro 5G",
+    "company": "Oppo",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/232190/oppo-find-x3-pro-den-1-org.jpg",
+    "price": "26.990.000",
+    "star": 4,
+    "rateCount": 68,
+    "promo": {
+        "name": "moiramat",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "AMOLED, 6.7', Quad HD+ (2K+)",
+        "os": "Android 11",
+        "camara": "Chính 50 MP & Phụ 50 MP, 13 MP, 3 MP",
+        "camaraFront": "32 MP",
+        "chip": "QSnapdragon 888",
+        "ram": "12 GB",
+        "rom": "256 GB",
+        "sim": "2 Nano SIM, Hỗ trợ 5G",
+        "battery": "4500 mAh, 65W"
+    },
+    "masp": "Opp1"
+}, {
+    "name": "Samsung Galaxy M51",
+    "company": "Samsung",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/217536/galaxym51-black-1020x680-org.jpg",
+    "price": "7.490.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "2.000.000"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.7', Full HD+",
+        "os": "Android 10",
+        "camara": "Chính 64 MP & Phụ 12 MP, 5 MP, 5 MP",
+        "camaraFront": "32 MP",
+        "chip": "Snapdragon 730",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "sim": "2 Nano SIM, Hỗ trợ 4G",
+        "battery": "7000 mAh, 25W"
+    },
+    "masp": "Sam2"
+}, {
+    "name": "iPhone XR 64GB ",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/190325/iphone-xr-cam-1-1-org.jpg",
+    "price": "12.490.000",
+    "star": 4,
+    "rateCount": 374,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.1', Liquid Retina",
+        "os": "iOS 14",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "chip": "Apple A12 Bionic",
+        "ram": "3 GB",
+        "rom": "64 GB",
+        "sim": "1 Nano SIM & 1 eSIM, Hỗ trợ 4G",
+        "battery": "2942 mAh, 15W"
+    },
+    "masp": "App1"
+}, {
+    "name": "iPhone 11 64GB",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/153856/iphone-11-vang-1-2-org.jpg",
+    "price": "17.490.000",
+    "star": 4,
+    "rateCount": 375,
+    "promo": {
+        "name": "giareonline",
+        "value": "14.990.000"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.1', Liquid Retina",
+        "os": "iOS 14",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "12 MP",
+        "chip": "Apple A13 Bionic",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "sim": "1 Nano SIM & 1 eSIM, Hỗ trợ 4G",
+        "battery": "3110 mAh, 18W"
+    },
+    "masp": "App2"
+}, {
+    "name": "Xiaomi Mi 8",
+    "company": "Xiaomi",
+    "img": "img/products/xiaomi-mi-8-1-600x600.jpg",
+    "price": "12.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "chip": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "sim": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Xia1"
+}, {
+    "name": "Samsung Galaxy Z Flip3 5G 128GB",
+    "company": "Samsung",
+    "img": "https://cdn.tgdd.vn/2021/08/campaign/Group4537(1)-599x475.png",
+    "price": "24.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "	Dynamic AMOLED 2X, Chính 6.7' & Phụ 1.9', Full HD+",
+        "os": "Android 11",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "10 MP",
+        "chip": "Snapdragon 888",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "sim": "Không",
+        "battery": "3300 mAh, 15 W"
+    },
+    "masp": "Xia2"
+}, {
+    "name": "Xiaomi Redmi 5 Plus 4GB",
+    "company": "Xiaomi",
+    "img": "img/products/xiaomi-redmi-5-plus-600x600.jpg",
+    "price": "4.790.000",
+    "star": 4,
+    "rateCount": 347,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "IPS LCD, 5.99', Full HD+",
+        "os": "Android 7.1 (Nougat)",
+        "camara": "12 MP",
+        "camaraFront": "5 MP",
+        "chip": "Snapdragon 625 8 nhân 64-bit",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "sim": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "4000 mAh"
+    },
+    "masp": "Xia3"
+}, {
+    "name": "Realme 8 Pro Vàng Rực Rỡ ",
+    "company": "Realme",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/241017/realme-8-pro--vang-1-org.jpg",
+    "price": "8.690.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.4', Full HD+",
+        "os": "Android 11",
+        "camara": "Chính 108 MP & Phụ 8 MP, 2 MP, 2 MP",
+        "camaraFront": "16 MP",
+        "chip": "Snapdragon 720G",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "sim": "2 Nano SIM, Hỗ trợ 4G",
+        "battery": "4500 mAh, 50W"
+    },
+    "masp": "Real0"
+}, {
+    "name": "Realme 8 Pro ",
+    "company": "Realme",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/235986/realme-8-pro-xanh-duong-1-org.jpg",
+    "price": "8.690.000",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "giareonline",
+        "value": "8.290.000"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.4', Full HD+",
+        "os": "Android 11",
+        "camara": "Chính 108 MP & Phụ 8 MP, 2 MP, 2 MP",
+        "camaraFront": "16 MP",
+        "chip": "Snapdragon 720G",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "sim": "2 Nano SIM, Hỗ trợ 4G",
+        "battery": "4500 mAh, 50W"
+    },
+    "masp": "Real1"
+}, {
+    "name": "Realme 7 Pro",
+    "company": "Realme",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/227689/realme-7-pro-bac-1-org.jpg",
+    "price": "7.470.000",
+    "star": 5,
+    "rateCount": 54,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6.4', Full HD+",
+        "os": "Android 10",
+        "camara": "Chính 64 MP & Phụ 8 MP, 2 MP, 2 MP",
+        "camaraFront": "32 MP",
+        "chip": "Snapdragon 720G",
+        "ram": "8 GB",
+        "rom": "128 GB",
+        "sim": "2 Nano SIM, Hỗ trợ 4G",
+        "battery": "4500 mAh, 65W"
+    },
+    "masp": "Real2"
+}, {
+    "name": "Samsung Galaxy S21 Ultra 5G 128GB",
+    "company": "Samsung",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/226316/samsung-galaxy-s21-ultra-bac-1-org.jpg",
+    "price": "25.990.000",
+    "star": 3,
+    "rateCount": 89,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Dynamic AMOLED 2X, 6.8', Quad HD+ (2K+)",
+        "os": "Android 11",
+        "camara": "Chính 108 MP & Phụ 12 MP, 10 MP, 10 MP",
+        "camaraFront": "40 MP",
+        "chip": "Exynos 2100",
+        "ram": "12 GB",
+        "rom": "128 GB",
+        "sim": "2 Nano SIM hoặc 1 Nano SIM + 1 eSIM, Hỗ trợ 5G",
+        "battery": "5000 mAh, 25W"
+    },
+    "masp": "Sam3"
+}, {
+    "name": "iPhone 12 mini 64GB ",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/225380/iphone-12-mini-xanh-la-1-1-org.jpg",
+    "price": "18.990.000",
+    "star": 3,
+    "rateCount": 387,
+    "promo": {
+        "name": "giareonline",
+        "value": "16.490.000"
+    },
+    "detail": {
+        "screen": "OLED, 5.4', Super Retina XDR",
+        "os": "iOS 14",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "12 MP",
+        "chip": "Apple A14 Bionic",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "sim": "1 Nano SIM & 1 eSIM, Hỗ trợ 5G",
+        "battery": "2227 mAh, 20W"
+    },
+    "masp": "App3"
+}, {
+    "name": "iPhone SE 64GB (2020) (Hộp mới)",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/230410/iphone-se-64gb-2020-hop-moi-do-1-1-org.jpg",
+    "price": "10.490.000",
+    "star": 5,
+    "rateCount": 154,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 4.7', Retina HD",
+        "os": "iOS 14",
+        "camara": "12 MP",
+        "camaraFront": "7 MP",
+        "chip": "Apple A13 Bionic",
+        "ram": "3 GB",
+        "rom": "64 GB",
+        "sim": "1 Nano SIM & 1 eSIM, Hỗ trợ 4G",
+        "battery": "1821 mAh, 20W"
+    },
+    "masp": "App4"
+}, {
+    "name": "iPhone 12 Pro 128GB ",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/213032/iphone-12-pro-bac-1-org.jpg",
+    "price": "30.990.000",
+    "star": 5,
+    "rateCount": 235,
+    "promo": {
+        "name": "giareonline",
+        "value": "27.990.000"
+    },
+    "detail": {
+        "screen": "OLED, 6.1', Super Retina XDR",
+        "os": "iOS 14",
+        "camara": "3 camera 12 MP",
+        "camaraFront": "12 MP",
+        "chip": "Apple A14 Bionic",
+        "ram": "6 GB",
+        "rom": "128 GB",
+        "sim": "1 Nano SIM & 1 eSIM, Hỗ trợ 5G",
+        "battery": "2815 mAh, 20W"
+    },
+    "masp": "App5"
+}, {
+    "name": "iPhone 12 64GB",
+    "company": "Apple",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/213031/iphone-12-violet-gc-org.jpg",
+    "price": "22.990.000",
+    "star": 4,
+    "rateCount": 412,
+    "promo": {
+        "name": "giareonline",
+        "value": "20.490.000"
+    },
+    "detail": {
+        "screen": "OLED, 6.1', Super Retina XDR",
+        "os": "iOS 14",
+        "camara": "2 camera 12 MP",
+        "camaraFront": "12 MP",
+        "chip": "Apple A14 Bionic",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "sim": "1 Nano SIM & 1 eSIM, Hỗ trợ 5G",
+        "battery": "2815 mAh, 20W"
+    },
+    "masp": "App6"
+}, {
+    "name": "Samsung Galaxy Note 20",
+    "company": "Samsung",
+    "img": "https://cdn.tgdd.vn/Products/Images/42/218355/samsung-galaxy-note-20-vang-dong-1-org.jpg",
+    "price": "14.990.000",
+    "star": 3,
+    "rateCount": 89,
+    "promo": {
+        "name": "giamgia",
+        "value": "9.000.000"
+    },
+    "detail": {
+        "screen": "Super AMOLED Plus, 6.7', Full HD+",
+        "os": "Android 10",
+        "camara": "Chính 12 MP & Phụ 64 MP, 12 MP",
+        "camaraFront": "10 MP",
+        "chip": "Exynos 990",
+        "ram": "8 GB",
+        "rom": "256 GB",
+        "sim": "2 Nano SIM hoặc 1 Nano SIM + 1 eSIM, Hỗ trợ 4G",
+        "battery": "4300 mAh, 25W"
+    },
+    "masp": "Sam4"
+
+}]
